62113f66e3972a76010e58d96b0da8c98677ec3f
B-Nilson <nilson.brayden@gmail.com>
2025-11-09 12:20:01 -0800

    cleanup naps file downloading/reading

--- R/get_naps_data.R
+++ R/get_naps_data.R
@@ -17,10 +17,38 @@ get_naps_data <- function(years, pollutants = "PM25") {
           .return = NULL,
           withr::with_options(
             list(timeout = 6000),
-            data.table::fread(url, skip = 7)
+            {
+              local_file <- tempdir() |>
+                file.path(
+                  basename(url) |> sub(pattern = ".*%2F", replacement = "")
+                )
+              if (!file.exists(local_file)) {
+                download.file(url, local_file, method = "libcurl", quiet = TRUE)
+              }
+              read_naps_csv(local_file)
+            }
           )
         )
       },
-      .bind = TRUE
+      .as_list = TRUE
+    ) |>
+    stats::setNames(
+      file_paths |> basename() |> gsub(pattern = ".csv", replacement = "")
     )
+}
+
+read_naps_csv <- function(csv_file) {
+  data_header_row <- readLines(csv_file, n = 10) |>
+    stringr::str_which(pattern = "H\\d\\d?//H\\d\\d?")
+  header <- csv_file |>
+    readLines(n = data_header_row - 1) |>
+    stringr::str_split(pattern = ", ?", simplify = TRUE) |>
+    stats::setNames(c("label", "value")) |>
+    dplyr::as_tibble() |>
+    dplyr::select(label = V1, value = V2)
+
+  data <- readLines(csv_file)[-(1:(data_header_row - 1))] |>
+    paste(collapse = "\n") |>
+    data.table::fread(keepLeadingZeros = TRUE)
+  list(header = header, data = data)
 }
