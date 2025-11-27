d2c58d2c218d4bb77d91ac9c064c5defca0d66e5
B-Nilson <nilson.brayden@gmail.com>
2025-11-27 13:16:48 -0800

    remove old PM25 specific test that has been replaced by general tests

--- tests/testthat/test-observations.R
+++ tests/testthat/test-observations.R
@@ -137,36 +137,3 @@ test_that("values are within expected ranges", {
 
   expect_true(nrow(bad_sites) == 0)
 })
-
-test_that("check for errors in PM25 data", {
-  db <- connect_to_database()
-  total_rows <- db |> dplyr::tbl("PM25") |> dplyr::tally() |> dplyr::pull(n)
-
-  # find/count negative values
-  negatives <- db |>
-    dplyr::tbl("PM25") |>
-    dplyr::filter(pm25 < 0) |>
-    dplyr::count(pm25) |>
-    dplyr::mutate(percent = n / total_rows * 100)
-
-  extremes <- db |>
-    dplyr::tbl("PM25") |>
-    dplyr::filter(pm25 > 2000) |>
-    dplyr::count(pm25) |>
-    dplyr::mutate(percent = n / total_rows * 100)
-
-  maybe_status_flag <- db |>
-    dplyr::tbl("PM25") |>
-    dplyr::filter(pm25 == 185) |>
-    dplyr::count(pm25) |>
-    dplyr::mutate(percent = n / total_rows * 100)
-
-  multiple_methods_same_time <- db |>
-    dplyr::tbl("PM25") |>
-    dplyr::group_by(site_id, date) |>
-    dplyr::count() |>
-    dplyr::ungroup() |>
-    dplyr::filter(n > 1) |>
-    dplyr::count() |>
-    dplyr::mutate(percent = n / total_rows * 100)
-})
