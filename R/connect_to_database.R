ca467ac610878fc630feaf4574b3eddebb958679
B-Nilson <nilson.brayden@gmail.com>
2025-11-27 14:53:57 -0800

    cleanup connect_to_database so works with custom paths

--- R/connect_to_database.R
+++ R/connect_to_database.R
@@ -1,11 +1,11 @@
 connect_to_database <- function(
-  db_name = "naps.duckdb",
-  db_path = system.file("extdata", db_name, package = "napsreview"),
+  db_path = system.file("extdata", package = "napsreview") |>
+    file.path("naps.duckdb"),
   create_if_needed = TRUE
 ) {
   if (!file.exists(db_path) & create_if_needed) {
     basename(db_path) |>
-      handyr::create_database(path = dirname(db_path))
+      handyr::create_database(path = dirname(db_path), type = "duckdb")
   } else {
     duckdb::duckdb() |>
       DBI::dbConnect(db_path)
