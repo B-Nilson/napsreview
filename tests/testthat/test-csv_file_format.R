0d48e5d2f3fdab099d9bc9db63c338a9180b620f
B-Nilson <nilson.brayden@gmail.com>
2025-11-27 14:24:31 -0800

    note file format differences

--- tests/testthat/test-csv_file_format.R
+++ tests/testthat/test-csv_file_format.R
@@ -1,4 +1,9 @@
 # Note: The file format changed after 2004, this should be rectified or at least acknowledged
+# Formats differences:
+# - new files have FR and EN headers
+# - new files are UTF-8, old files are latin1
+# - new files have slighlty different pre-data header format
+# - new files have a different date format
 test_that("file formats are consistent", {
   db <- connect_to_database()
   on.exit(DBI::dbDisconnect(db))
@@ -26,3 +31,5 @@ test_that("file formats are consistent", {
   v1_rows |> expect_equal(0)
   (v2_rows > 0) |> expect_true()
 })
+
+# TODO: add test showing how method_code is only in PM2.5 files ?
